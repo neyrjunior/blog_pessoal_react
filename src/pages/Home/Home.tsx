17a3fb23f7f9cff3b29461ef2fb0fb84ed1025ac
Ney Robson <nrobson018@gmail.com>
2024-08-05 21:17:14 -0300

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Eu sou o samba</h1>
+            <h2>A voz do morro, sou eu mesmo sim sehor</h2>
+            <img src="https://media.licdn.com/dms/image/D4D03AQG54VxeJIRnfg/profile-displayphoto-shrink_800_800/0/1719343721747?e=1728518400&v=beta&t=ggHbQWfIYH6QfOM6hlu2q3Re7jP3bnJpH6TH0eJFXrE" alt="" />
+        </>
+    )
+}
+export default Home;
