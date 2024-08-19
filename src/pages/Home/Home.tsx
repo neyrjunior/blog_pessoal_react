a3d1419ce2fc8b936e3781c13fc638eab00a8102
Ney Robson <nrobson018@gmail.com>
2024-08-19 12:10:25 -0300

    deploy do projeto

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,35 @@
-import { useNavigate, Link } from 'react-router-dom'
+import React from 'react';
+import homeLogo from '../../assets/home.png'
+import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
 
-const Home = () => {
-    let navigate = useNavigate()
+
+function Home() {
     return (
-
-        <div className='v-96'>
-            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
-            <div>
-                <button type='submit'
-                    className='hover:underline mx-4'
-                    onClick={() => { navigate('/login') }}>
-                    
-                </button>
-                <Link to='/login' className='hover:underline mx-4'></Link>
+        <>
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
             </div>
-
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
+            </div>
+          </div>
         </div>
-
-    )
+        <ListaPostagens />
+      </>
+    );
 }
 
-export default Home
+export default Home;
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'https://blogpessoal-mand.onrender.com'
+export const api = axios.create({
+  baseURL: import.meta.env.BASE_URL
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
